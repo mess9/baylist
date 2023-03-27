65d5ee56a95acbb3a9a1b9cfd6c0aa960fa4e954
Артем-PC <baikoff@bk.ru>
2023-03-27 19:44:33 +0300

    первый

---
+++ src/main/java/org/baylist/BaylistApplication.java
@@ -0,0 +1,13 @@
+package org.baylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaylistApplication.class, args);
+	}
+
+}
