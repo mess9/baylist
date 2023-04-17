7b4a3cb24441d4a62261986cbe8d692d268bba04
Artem <baikoff@bk.ru>
2023-04-17 18:25:02 +0300

    Рубрика Эксперименты

--- src/main/java/org/baylist/BayListApplication.java
+++ src/main/java/org/baylist/BayListApplication.java
@@ -8,6 +8,7 @@ public class BayListApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		System.out.println("Hello world22");
 		SpringApplication.run(BayListApplication.class, args);
 	}
 
