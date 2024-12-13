55bded8c6953f4625cc4c692a7fd8ba5cf32e380
fil <as0509@ya.ru>
2024-12-13 15:50:56 +0400

    тест автозапуска сервиса(4)

--- src/main/java/org/baylist/BayListApplication.java
+++ src/main/java/org/baylist/BayListApplication.java
@@ -8,7 +8,7 @@ public class BayListApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BayListApplication.class, args);
-		System.out.println("buylist started");
+		System.out.println("buylist started again");
 	}
 
 
