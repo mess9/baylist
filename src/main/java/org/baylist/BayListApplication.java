cfd61c26b78d3e5d7ac54659eb0a43166140aa7a
fil <as0509@ya.ru>
2024-12-13 12:15:29 +0400

    попытки в деплой(7)

--- src/main/java/org/baylist/BayListApplication.java
+++ src/main/java/org/baylist/BayListApplication.java
@@ -11,6 +11,7 @@ public class BayListApplication {
 		System.out.println("Hello World!");
 		System.out.println("Hello World!");
 		System.out.println("Hello World!");
+		System.out.println("Hello World!");
 	}
 
 
