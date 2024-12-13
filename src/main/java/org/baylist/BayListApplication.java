50d7c3e8121108ff909b458c5ad5a9024373c829
fil <as0509@ya.ru>
2024-12-13 14:17:39 +0400

    попытки в деплой(21)

--- src/main/java/org/baylist/BayListApplication.java
+++ src/main/java/org/baylist/BayListApplication.java
@@ -8,10 +8,7 @@ public class BayListApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BayListApplication.class, args);
-		System.out.println("Hello World!");
-		System.out.println("Hello World!");
-		System.out.println("Hello World!");
-		System.out.println("Hello World!");
+		System.out.println("buylist started");
 	}
 
 
