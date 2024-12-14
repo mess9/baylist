a9f549643457cc193a20794d00b519d257138ac1
fil <as0509@ya.ru>
2024-12-14 15:31:30 +0400

    хелс чеки(3)

--- src/main/java/org/baylist/todoist/api/controller/TestApi.java
+++ src/main/java/org/baylist/todoist/api/controller/TestApi.java
@@ -1,21 +1,16 @@
 package org.baylist.todoist.api.controller;
 
 import lombok.AllArgsConstructor;
-import org.baylist.todoist.api.Todoist;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestClient;
 
 @RestController("/")
 @AllArgsConstructor
 public class TestApi {
 
-    private final Todoist todoist;
-    private final RestClient restClient;
-
     @GetMapping("/test")
     public String test() {
-        return "test";
+        return "make love not war";
     }
 
 
