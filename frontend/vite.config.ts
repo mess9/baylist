a28e13799ffe7984402d679d2690b85f827901ec
tea06194 <tea06194@gmail.com>
2024-12-18 23:54:24 +0300

    add postcss-nesting, aliases, @types/node

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
 import {defineConfig} from 'vite'
 import preact from '@preact/preset-vite'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+    resolve: {
+        alias: {
+            src: path.resolve(__dirname, './src'),
+            app: path.resolve(__dirname, './app'),
+            pages: path.resolve(__dirname, './pages'),
+        }
+    },
     plugins: [preact()],
 })
