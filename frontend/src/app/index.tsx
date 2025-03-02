18d7d88e67b641ab46d6a22c35c58c4282ee3873
tea06194 <tea06194@gmail.com>
2025-03-02 21:27:08 +0300

    try miniapp

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -15,6 +15,15 @@ export default function App() {
   onMount(() => {
     tg?.expand();
   });
+
+  console.dir(tg.initDataUnsafe)
+  const initDataUnsafe = tg.initDataUnsafe;
+
+  const reqQP = { id: initDataUnsafe?.user?.id, first_name: initDataUnsafe?.user?.first_name, username: initDataUnsafe?.user?.username, auth_data: initDataUnsafe?.auth_date, hash: initDataUnsafe?.hash }
+
+  fetch(`http://152.70.174.74:8080/api/auth/telegram?id=${reqQP?.id}&first_name=${reqQP?.first_name}&username=${reqQP?.username}&auth_data=${reqQP?.auth_data}&hash=${reqQP?.hash}`, {method: "POST"})
+	.then(res=>res.json()).then(res=>console.log(res.token))
+	.catch(err=>console.log(err, "Im Err"));
   return (
     <ErrorBoundary
       fallback={(err) => (console.log(err), (<span>{`${err}`}</span>))}
